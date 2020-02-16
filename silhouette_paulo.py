a5f5f1a4be081f2be26609e66099ad944cf11f44
Paulo Renato <mendesdpn@gmail.com>
2020-02-16 17:46:43 -0300

    added evaluation

--- silhouette_paulo.py
+++ silhouette_paulo.py
@@ -1,34 +1,4 @@
-"""
-===============================================================================
-Selecting the number of clusters with silhouette analysis on KMeans clustering
-===============================================================================
 
-Silhouette analysis can be used to study the separation distance between the
-resulting clusters. The silhouette plot displays a measure of how close each
-point in one cluster is to points in the neighboring clusters and thus provides
-a way to assess parameters like number of clusters visually. This measure has a
-range of [-1, 1].
-
-Silhouette coefficients (as these values are referred to as) near +1 indicate
-that the sample is far away from the neighboring clusters. A value of 0
-indicates that the sample is on or very close to the decision boundary between
-two neighboring clusters and negative values indicate that those samples might
-have been assigned to the wrong cluster.
-
-In this example the silhouette analysis is used to choose an optimal value for
-``n_clusters``. The silhouette plot shows that the ``n_clusters`` value of 3, 5
-and 6 are a bad pick for the given data due to the presence of clusters with
-below average silhouette scores and also due to wide fluctuations in the size
-of the silhouette plots. Silhouette analysis is more ambivalent in deciding
-between 2 and 4.
-
-Also from the thickness of the silhouette plot the cluster size can be
-visualized. The silhouette plot for cluster 0 when ``n_clusters`` is equal to
-2, is bigger in size owing to the grouping of the 3 sub clusters into one big
-cluster. However when the ``n_clusters`` is equal to 4, all the plots are more
-or less of similar thickness and hence are of similar sizes as can be also
-verified from the labelled scatter plot on the right.
-"""
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
@@ -115,8 +85,8 @@ def analyze(X, range_n_clusters, show_individual_graphs = True):
             plt.title(("Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"%n_clusters),fontsize=14, fontweight='bold')
             
 
-    plt.title(("Silhouette score for each cluster number"),fontsize=16, fontweight='bold')
-    fig, ax1 = plt.subplots(1,1)
+    fig, ax1 = plt.subplots(1)
+    ax1.set_title(("Silhouette score for each cluster number"),fontsize=16, fontweight='bold')
     fig.set_size_inches(18, 7)
     ax1.plot(x_plot , y_plot)
     ax1.set_xticks([i+1 for i in range(max(range_n_clusters))])
