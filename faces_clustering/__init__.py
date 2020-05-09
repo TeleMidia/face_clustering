384cfea103bfd8693c924bbea37f38c78d9f9cf8
Paulo Renato <mendesdpn@gmail.com>
2020-05-09 18:23:19 -0300

    video

--- faces_clustering/__init__.py
+++ faces_clustering/__init__.py
@@ -2,5 +2,6 @@ from .feature_extractor import FeatureExtractor
 from .face_searcher import FaceSearcher
 from .clustering import Clusterer
 from .utils import is_image, get_files_folder, display_image
+from .silhouette_paulo import silhuoette
 
-__all__ = ['FeatureExtractor', 'Clusterer', 'is_image', 'get_files_folder', 'display_image', 'FaceSearcher']
+__all__ = ['FeatureExtractor', 'Clusterer', 'is_image', 'get_files_folder', 'display_image', 'FaceSearcher', 'silhuoette']
--- faces_clustering/silhouette_paulo.py
+++ faces_clustering/silhouette_paulo.py
@@ -10,7 +10,7 @@ import matplotlib.cm as cm
 import numpy as np
 
 
-def analyze(X, range_n_clusters, show_individual_graphs = True):
+def silhuoette(X, range_n_clusters):
     x_plot = []
     silhuette_plot = []
     sse_plot = []
@@ -32,74 +32,21 @@ def analyze(X, range_n_clusters, show_individual_graphs = True):
 
         #print(f"Clusters ={n_clusters} Silhouette: {silhouette_avg} SSE: {clusterer.inertia_}")
         x_plot.append(n_clusters)
-        silhuette_plot.append(silhouette_avg)
+        silhuette_plot.append(silhouette_avg)        
         
-        if(show_individual_graphs):        
-            # Create a subplot with 1 row and 2 columns
-            fig, ax1 = plt.subplots(1,1)
-            fig.set_size_inches(18, 7)
-
-            # The 1st subplot is the silhouette plot
-            # The silhouette coefficient can range from -1, 1 
-            ax1.set_xlim([-1, 1])
-            # The (n_clusters+1)*10 is for inserting blank space between silhouette
-            # plots of individual clusters, to demarcate them clearly.
-            ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
-
-        
-     
-            # Compute the silhouette scores for each sample
-            sample_silhouette_values = silhouette_samples(X, cluster_labels)
-
-            y_lower = 10
-            for i in range(n_clusters):
-                # Aggregate the silhouette scores for samples belonging to
-                # cluster i, and sort them
-                ith_cluster_silhouette_values = \
-                    sample_silhouette_values[cluster_labels == i]
-
-                ith_cluster_silhouette_values.sort()
-
-                size_cluster_i = ith_cluster_silhouette_values.shape[0]
-                y_upper = y_lower + size_cluster_i
-
-                color = cm.nipy_spectral(float(i) / n_clusters)
-                ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                                  0, ith_cluster_silhouette_values,
-                                  facecolor=color, edgecolor=color, alpha=0.7)
-
-                # Label the silhouette plots with their cluster numbers at the middle
-                ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-                # Compute the new y_lower for next plot
-                y_lower = y_upper + 10  # 10 for the 0 samples
-
-            ax1.set_title("The silhouette plot for the various clusters.")
-            ax1.set_xlabel("The silhouette coefficient values")
-            ax1.set_ylabel("Cluster label")
-
-            # The vertical line for average silhouette score of all the values
-            ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
-
-            ax1.set_yticks([])  # Clear the yaxis labels / ticks
-            ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-
-            plt.title(("Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"%n_clusters),fontsize=14, fontweight='bold')
-            
 
     fig, ax1 = plt.subplots(1)
     ax1.set_title(("Silhouette score for each cluster number"),fontsize=16, fontweight='bold')
     fig.set_size_inches(18, 7)
     silhuette_plot = silhuette_plot/max(silhuette_plot)
-    ax1.plot(x_plot , silhuette_plot)
+    ax1.plot(x_plot , silhuette_plot, label='silhuoette')
     sse_plot = sse_plot/max(sse_plot)
-    ax1.plot(x_plot , sse_plot)
+    ax1.plot(x_plot , sse_plot, label='inertia')
     ax1.set_xticks([i+1 for i in range(max(range_n_clusters))])
+    ax1.legend()
 
     plt.grid(b=True, which='major', color='#666666', linestyle='-')
     # Show the minor grid lines with very faint and almost transparent grey lines
     plt.minorticks_on()
     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
     plt.show()
-    return sihuetter_plot
