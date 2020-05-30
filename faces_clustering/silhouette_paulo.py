f9e6ad263a378c279fb1be00aa6c5bcbfde5c415
Paulo Renato <mendesdpn@gmail.com>
2020-05-29 23:37:07 -0300

    updated silhouette score for video

--- faces_clustering/silhouette_paulo.py
+++ faces_clustering/silhouette_paulo.py
@@ -1,6 +1,7 @@
 
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.metrics.cluster import homogeneity_score
 from tqdm.notebook import tqdm
@@ -10,43 +11,58 @@ import matplotlib.cm as cm
 import numpy as np
 
 
-def silhuoette(X, range_n_clusters):
+def silhuoette(X, alg = "kmeans", max_dec = 5):
+
+    assert alg in ['agglomerative', 'kmeans'], "alg must be kmeans or agglomerative"
+
     x_plot = []
     silhuette_plot = []
     sse_plot = []
-    print(f'Computing clusters from {min(range_n_clusters)} to {max(range_n_clusters)}')
-    for n_clusters in tqdm(range_n_clusters):
-        
-        # Initialize the clusterer with n_clusters value and a random generator
-        # seed of 10 for reproducibility.
-        clusterer = KMeans(n_clusters=n_clusters, random_state=42)
-        cluster_labels = clusterer.fit_predict(X)
 
-        # The silhouette_score gives the average value for all the samples.
-        # This gives a perspective into the density and separation of the formed
-        # clusters
-        silhouette_avg = silhouette_score(X, cluster_labels)
-        sse_plot.append(clusterer.inertia_)
+    max_silhouette = -1
 
-        #homo_score = homogeneity_score(true_labels, cluster_labels)
+    #x_plot.append(1)
+    #silhuette_plot.append(max_silhouette)   
 
-        #print(f"Clusters ={n_clusters} Silhouette: {silhouette_avg} SSE: {clusterer.inertia_}")
-        x_plot.append(n_clusters)
-        silhuette_plot.append(silhouette_avg)        
-        
+    num_dec = 0
+    n_clusters = 1
 
     fig, ax1 = plt.subplots(1)
     ax1.set_title(("Silhouette score for each cluster number"),fontsize=16, fontweight='bold')
     fig.set_size_inches(18, 7)
-    silhuette_plot = silhuette_plot/max(silhuette_plot)
-    ax1.plot(x_plot , silhuette_plot, label='silhuoette')
-    sse_plot = sse_plot/max(sse_plot)
-    ax1.plot(x_plot , sse_plot, label='inertia')
-    ax1.set_xticks([i+1 for i in range(max(range_n_clusters))])
-    ax1.legend()
-
     plt.grid(b=True, which='major', color='#666666', linestyle='-')
-    # Show the minor grid lines with very faint and almost transparent grey lines
     plt.minorticks_on()
     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+
+    hl, = plt.plot([], [])
+
+    while num_dec <= max_dec:
+        n_clusters+=1
+        if alg == 'agglomerative':
+            clusterer = AgglomerativeClustering(n_clusters=n_clusters)
+        elif alg == 'kmeans':
+            clusterer = KMeans(n_clusters=n_clusters, random_state=42)
+
+        cluster_labels = clusterer.fit_predict(X)
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        x_plot.append(n_clusters)
+        silhuette_plot.append(silhouette_avg)        
+
+        if silhouette_avg < max_silhouette:
+            num_dec += 1
+        else:
+            max_silhouette = silhouette_avg
+            num_dec = 0
+               
+
+    #silhuette_plot = silhuette_plot/max(silhuette_plot)
+    ax1.plot(x_plot , silhuette_plot, label='silhuoette')
+    #sse_plot = sse_plot/max(sse_plot)
+    #ax1.plot(x_plot , sse_plot, label='inertia')
+    ax1.set_xticks([i+1 for i in range(n_clusters)])
+    ax1.legend()
+    ax1.set_xlabel("Number of clusters")
+    ax1.set_ylabel("Silhouette score")
     plt.show()
+
+    return x_plot[np.argmax(silhuette_plot)]
